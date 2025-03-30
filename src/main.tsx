a80db04672b5489759069528c1d9cc4267e32941
Egozi <egozi@wix.com>
2025-03-30 15:57:22 +0300

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { WixDesignSystemProvider } from '@wix/design-system'
+import '@wix/design-system/styles.global.css'
 import App from './App.tsx'
+import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <WixDesignSystemProvider features={{ newColorsBranding: true }}>
+          <App />
+      </WixDesignSystemProvider>
   </StrictMode>,
 )
